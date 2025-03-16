47ea0edaefa2cb2c2cc6eab7376f1723ef69b3fc
Sofia-gb <sofiagomezb@yahoo.com.ar>
2025-03-16 18:45:02 -0300

    [DOC] add documentation to the generate-compose python script

--- generate_compose.py
+++ generate_compose.py
@@ -4,6 +4,8 @@ import yaml
 
 
 class ComposeGeneratorError(Enum):
+    """Represents the errors that can occur in the ComposeGenerator script."""
+
     ARGUMENT_COUNT_ERROR = (
         "El script de python debe ejecutarse de la siguiente manera: "
         "python3 generate_compose.py <archivo_salida> <cantidad_clientes>."
@@ -14,11 +16,14 @@ class ComposeGeneratorError(Enum):
 
 
 def exit_with_error(error):
+    """ Exits the script with the given error message. """
     sys.stderr.write(error.value + "\n")
     sys.exit(1)
 
 
 def get_arguments():
+    """ Returns the output file and the number of clients from the command line arguments. """
+
     EXPECTED_PARAMS = 3  # generate_compose.py <archivo_salida> <n_clientes>
 
     if len(sys.argv) != EXPECTED_PARAMS:
@@ -35,6 +40,10 @@ def get_arguments():
 
 
 def parse_arguments(output_file, number_of_clients):
+    """ Returns the output file and the number of clients as a tuple.
+    If the output file does not end with .yaml or .yml, the .yaml extension it is added. 
+    The number of clients is converted to an integer. """
+
     if not (output_file.endswith('.yaml') or output_file.endswith('.yml')):
         output_file += '.yaml'
 
@@ -42,6 +51,8 @@ def parse_arguments(output_file, number_of_clients):
 
 
 def config_clients(compose, number_of_clients):
+    """ Configures the clients in the compose file. 
+     The number of clients is determined by the number_of_clients parameter. """
 
     for i in range(1, number_of_clients + 1):
         compose["services"][f"client{i}"] = {
@@ -59,6 +70,8 @@ def config_clients(compose, number_of_clients):
 
 
 def config_networks(compose):
+    """ Configures the network in the compose file. """
+
     compose["networks"] = {
         "testing_net": {
             "ipam": {
@@ -74,6 +87,8 @@ def config_networks(compose):
 
 
 def config_server(compose):
+    """ Configures the server in the compose file. """
+
     compose["services"]["server"] = {
             "container_name": "server",
             "image": "server:latest",
@@ -87,12 +102,17 @@ def config_server(compose):
 
 
 def config_services(compose, number_of_clients):
+    """ Configures the services in the compose file. This includes the server and the clients. """
+
     compose["services"] = {}
     config_server(compose)
     config_clients(compose, number_of_clients)
 
 
 def generate_compose(output_file, number_of_clients):
+    """ Generates the compose file with the given number of clients. 
+    This file is saved with the given output file name.
+    """
 
     compose = {"name": "tp0"}
     config_services(compose, number_of_clients)
